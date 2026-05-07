4f9987cf99429fcd91ac9c8a5884c8917385ec39
Prosperi12 <138193982+Prosperi12@users.noreply.github.com>
2026-05-07 11:40:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-
-// Serve your HTML/CSS frontend
 app.use(express.static(path.join(__dirname, "public")));
 
-// Test backend route
 app.get("/api/health", (req, res) => {
   res.json({ message: "Backend is working" });
 });
